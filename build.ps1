5533bca7dd7b8a12199a4278e8fe127a7d00269e
Nitridan <andrew.levin77@hotmail.com>
2016-04-09 09:02:10 +0300

    Add option to build with local dotnet SDK.

--- build.ps1
+++ build.ps1
@@ -1,12 +1,23 @@
-param($buildNumber)
+param($buildNumber,
+    [switch]
+    $localDotNet)
 
 $ELECTRON_VERSION=0.37.5
+$DOTNET_SDK_URL=https://dotnetcli.blob.core.windows.net/dotnet/beta/Binaries/Latest/dotnet-dev-win-x64.latest.zip
+$ATOM_SHELL_URL=https://atom.io/download/atom-shell
 
 if (!$buildNumber){
     $buildNumber = 1
 }
 
 $NATIVE_VERSION='1.0.' + $buildNumber
+
+# If specified we will prepend PATH with local dotnet CLI SDK
+if ($localDotNet){
+    Invoke-WebRequest -Uri $DOTNET_SDK_URL -OutFile dotnet.zip
+    Expand-Archive -Path dotnet.zip -DestinationPath dist-dotnet -Force
+    $env:PATH=(Convert-Path .) + '\dist-dotnet;' + $env:PATH
+}
 
 function Force-Copy($source, $destination){
     New-Item -ItemType File -Path $destination -Force
@@ -39,9 +50,9 @@ $electronX86binaryTarget = $electronDistributiveDir + '\ClrLoader_x86.node'
 $electronX64binaryTarget = $electronDistributiveDir + '\ClrLoader_x64.node'
 Set-Location $nodenativeDir
 & npm install
-& node-gyp rebuild --arch=x64 --target=$ELECTRON_VERSION --dist-url=https://atom.io/download/atom-shell
+& node-gyp rebuild --arch=x64 --target=$ELECTRON_VERSION --dist-url=$ATOM_SHELL_URL
 Force-Copy $outputPath $electronX86binaryTarget
-& node-gyp rebuild --arch=ia32 --target=$ELECTRON_VERSION --dist-url=https://atom.io/download/atom-shell
+& node-gyp rebuild --arch=ia32 --target=$ELECTRON_VERSION --dist-url=$ATOM_SHELL_URL
 Force-Copy $outputPath $electronX64binaryTarget
 Get-ChildItem -path $nodenativeDir | Where { $_.Extension -eq '.js' } `
     | Copy-Item -destination {$_.FullName -replace [System.Text.RegularExpressions.Regex]::Escape($nodenativeDir),$electronDistributiveDir} -Force
